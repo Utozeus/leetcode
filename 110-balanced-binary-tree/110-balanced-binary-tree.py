8ebc7a1fc1a916dfcc4989763bf18924b34d4b23
ajpapa <97355340+ajpapa@users.noreply.github.com>
2022-03-15 00:44:49 +0900

    Time: 61 ms (47.82%), Space: 17.7 MB (73.36%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -14,29 +14,11 @@ class Solution(object):
             return True
         else :
             return self.isBalanced(root.left) and self.isBalanced(root.right) and (abs(self.heightFind(root.left)-self.heightFind(root.right))<=1) 
-            
-            
-#             root.left=None and root.right=None :
-#             return True
-#         if root.left=None and root.right=None :
-#             return 0
-#         elif root.left=None and root.right!=None:
-#             return 1+self.heightFind(root.right)
-#         elif root.left!=None and root.right=None:
-#             return 1+self.heightFind(root.left)
-#         else :
-#             return 1+max(self.heightFind(root.left), self.heightFind(root.right))
 
     @classmethod
     def heightFind(self, root):
         if root==None:
             return 0
-#         elif root.left=None and root.right=None :
-#             return 0
-#         elif root.left=None and root.right!=None:
-#             return 1+self.heightFind(root.right)
-#         elif root.left!=None and root.right=None:
-#             return 1+self.heightFind(root.left)
         else :
             return 1+max(self.heightFind(root.left), self.heightFind(root.right))
         
