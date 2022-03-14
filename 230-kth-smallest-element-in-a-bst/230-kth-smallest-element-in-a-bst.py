4e0267f0f1b3d0c25cf7a141e77ac3068900b084
ajpapa <97355340+ajpapa@users.noreply.github.com>
2022-03-15 02:21:54 +0900

    Time: 65 ms (37.28%), Space: 21.4 MB (9.57%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution(object):
     def kthSmallest(self, root, k):
         """
@@ -14,37 +16,68 @@ class Solution(object):
         if root==None:
             return None
         
+        num=self.numberOfNodes(root.left)
         
-        visitList=[] #for in-order stack
+        if num > k-1 :
+            return self.kthSmallest(root.left,k)
+        elif num==k-1 :
+            return root.val
+        else : # num < k -1
+            return self.kthSmallest(root.right, k-num-1)
+        
                 
-        return self.help(root, visitList,k)[k-1]
-        
-        
     @classmethod    
-    def visit(self, root, visitList):
+    def numberOfNodes(self, root):
         if root==None:
-            return visitList
+            return 0
         else :
-            return visitList+[root.val]
-    
-    @classmethod
-    def help(self, root, visitList, k) :
+            return 1+self.numberOfNodes(root.left)+self.numberOfNodes(root.right)
+
+
+
+
+# First Solution
+# class Solution(object):
+#     def kthSmallest(self, root, k):
+#         """
+#         :type root: TreeNode
+#         :type k: int
+#         :rtype: int
+#         """
+#         if root==None:
+#             return None
         
-        if root==None:
-            return visitList
-        else :
-            if root.left!=None:
-                if len(visitList)==k:
-                    return visitList
-                else:
-                    visitList=self.help(root.left, visitList,k)
-                    visitList=self.visit(root,visitList)
-                    visitList=self.help(root.right, visitList,k)
-                    return visitList
-            else : #root.left==None
-                if len(visitList)==k:
-                    return visitList
-                else:
-                    visitList=self.visit(root, visitList)
-                    visitList=self.help(root.right, visitList,k)
-                    return visitList
+        
+#         visitList=[] #for in-order stack
+                
+#         return self.help(root, visitList,k)[k-1]
+        
+        
+#     @classmethod    
+#     def visit(self, root, visitList):
+#         if root==None:
+#             return visitList
+#         else :
+#             return visitList+[root.val]
+    
+#     @classmethod
+#     def help(self, root, visitList, k) :
+        
+#         if root==None:
+#             return visitList
+#         else :
+#             if root.left!=None:
+#                 if len(visitList)==k:
+#                     return visitList
+#                 else:
+#                     visitList=self.help(root.left, visitList,k)
+#                     visitList=self.visit(root,visitList)
+#                     visitList=self.help(root.right, visitList,k)
+#                     return visitList
+#             else : #root.left==None
+#                 if len(visitList)==k:
+#                     return visitList
+#                 else:
+#                     visitList=self.visit(root, visitList)
+#                     visitList=self.help(root.right, visitList,k)
+#                     return visitList
