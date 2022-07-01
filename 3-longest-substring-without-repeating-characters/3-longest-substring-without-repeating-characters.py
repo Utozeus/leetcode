013da45d53b5ab759156dfb78e3cf1f72441c392
ajpapa <97355340+ajpapa@users.noreply.github.com>
2022-07-01 21:57:39 +0900

    Time: 28 ms (99.61%), Space: 13.5 MB (85.78%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,27 +4,49 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        n=len(s)
         
-        ans=0 #maximum length
+        '''
+        version 1
+        '''
+#         n=len(s)
         
-        for i in range(n) :
+#         ans=0 #maximum length
+        
+#         for i in range(n) :
             
-            start=i
-            end=i
-            tempset={s[i]}
+#             start=i
+#             end=i
+#             tempset={s[i]}
             
-            same=False
-            while same is False :
-                templen_before=len(tempset) #reference
-                end=min(end+1, n-1) #increment 1 with upperbound
-                tempset.add(s[end])
-                templen_after=len(tempset) #reference
-                same=(templen_before==templen_after)
-                if ans < len(tempset) :
-                    ans = len(tempset)
+#             same=False
+#             while same is False :
+#                 templen_before=len(tempset) #reference
+#                 end=min(end+1, n-1) #increment 1 with upperbound
+#                 tempset.add(s[end])
+#                 templen_after=len(tempset) #reference
+#                 same=(templen_before==templen_after)
+#                 if ans < len(tempset) :
+#                     ans = len(tempset)
             
-        return ans
-                
-                
+#         return ans
+
+        '''
+        version 2
+        '''
+        
+        temp_dict={}
+        start=0
+        maxlen=0
+        
+        for i, c in enumerate(s) :
+            
+            if ( ( c in temp_dict ) and ( start <= temp_dict[c] ) ) :
+                start=temp_dict[c]+1
+            else :
+                maxlen=max(maxlen, i-start+1)
+            
+            
+            temp_dict[c]=i
+        
+        return maxlen
             
